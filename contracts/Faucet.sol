31a9303b7d99bd48d5765fa6b764205cffbd5e30
Gabriel Contarini <gcontarini@gmail.com>
2022-02-14 18:04:16 +0000

    Add token transfer to faucet

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./myERC20.sol";
 
 contract Faucet {
 
     address payable public owner;
+	address private token;
     
     constructor(address payable _owner) {
         owner = _owner;
@@ -18,10 +20,15 @@ contract Faucet {
 
     function transfer(address payable _to) public payable{
         _to.transfer(100000000000000000); //0.1 ether
+		myIERC20(token).mintFromFaucet(msg.sender, 1);
     }
 
     function getbalance() public view returns(uint) {
         return address(this).balance;
+    }
+
+	function setToken(address _token) external {
+        token = _token;
     }
 
     receive() external payable{
